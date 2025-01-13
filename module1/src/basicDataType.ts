c70010d291414d8d50d604d57cb6e87708d7ae6c
Nissan <dev.nissan@jsencoder.com>
2025-01-13 11:59:34 +0600

    spread used

--- module1/src/basicDataType.ts
+++ module1/src/basicDataType.ts
@@ -1,111 +1,113 @@
-// Basic data types
+{
+  // Basic data types
 
-// string
-let firstName = "John Doe";
+  // string
+  let firstName = "John Doe";
 
-// number
-let age: number = 25;
+  // number
+  let age: number = 25;
 
-// boolean
-let isStudent: boolean = true;
-// null
-let data: null = null;
+  // boolean
+  let isStudent: boolean = true;
+  // null
+  let data: null = null;
 
-// undefined
-let data2: undefined = undefined;
+  // undefined
+  let data2: undefined = undefined;
 
-// symbol
-let id: symbol = Symbol("id");
+  // symbol
+  let id: symbol = Symbol("id");
 
-// any
-let notSure: any = 4;
+  // any
+  let notSure: any = 4;
 
-// void
-function log(message: string): void {
-  console.log(message);
+  // void
+  function log(message: string): void {
+    console.log(message);
+  }
+
+  // never
+  function error(message: string): never {
+    throw new Error(message);
+  }
+
+  // object
+  let obj: object = {};
+
+  // array
+  let list: number[] = [1, 2, 3];
+
+  // tuple
+  let tuple: [string, number] = ["John Doe", 25];
+
+  // enum
+  enum Color {
+    Red,
+    Green,
+    Blue,
+  }
+  let c: Color = Color.Green;
+  console.log(c); // Output: 1
+
+  // Type assertions
+  let someValue: any = "Hello World";
+
+  let strLength: number = (someValue as string).length;
+
+  console.log(strLength); // Output: 11
+
+  // Type inference
+  let name1 = "John Doe";
+  let age1 = 25;
+  let isStudent1 = true;
+  let data1 = null;
+  let data3 = undefined;
+  let id1 = Symbol("id");
+  let bigNumber1 = 100n;
+  let notSure1 = 4;
+  let obj1 = {};
+  let list1 = [1, 2, 3];
+  let tuple1 = ["John Doe", 25];
+  enum Color1 {
+    Red,
+    Green,
+    Blue,
+  }
+  let c1 = Color.Green;
+  console.log(c1); // Output: 1
+  let someValue1 = "Hello World";
+  let strLength1 = (someValue as string).length;
+  console.log(strLength1); // Output: 11
+
+  // reference type---> object
+  type Person = {
+    readonly company?: "JS ENCODER"; //literal type  and optional type // readonly type when it not be changeable
+    name: string;
+    age?: number; //optional properties
+    isStudent: boolean;
+  };
+
+  let person: Person = {
+    company: "JS ENCODER",
+    name: "John Doe",
+    age: 25,
+    isStudent: true,
+  };
+
+  let user: Person = {
+    name: "nissan",
+    isStudent: false,
+  };
+
+  person.company = "JS ENCODER";
+
+  const addBalance = (num1: number, num2: number) => num1 + num2;
+  // same variable declare error though it on another page
+  const poorUser = {
+    name: "Niloy",
+    balance: 1000,
+    addBalance(balance: number) {
+      return `my current balance is ${this.balance + balance}`;
+    },
+  };
 }
-
-// never
-function error(message: string): never {
-  throw new Error(message);
-}
-
-// object
-let obj: object = {};
-
-// array
-let list: number[] = [1, 2, 3];
-
-// tuple
-let tuple: [string, number] = ["John Doe", 25];
-
-// enum
-enum Color {
-  Red,
-  Green,
-  Blue,
-}
-let c: Color = Color.Green;
-console.log(c); // Output: 1
-
-// Type assertions
-let someValue: any = "Hello World";
-
-let strLength: number = (someValue as string).length;
-
-console.log(strLength); // Output: 11
-
-// Type inference
-let name1 = "John Doe";
-let age1 = 25;
-let isStudent1 = true;
-let data1 = null;
-let data3 = undefined;
-let id1 = Symbol("id");
-let bigNumber1 = 100n;
-let notSure1 = 4;
-let obj1 = {};
-let list1 = [1, 2, 3];
-let tuple1 = ["John Doe", 25];
-enum Color1 {
-  Red,
-  Green,
-  Blue,
-}
-let c1 = Color.Green;
-console.log(c1); // Output: 1
-let someValue1 = "Hello World";
-let strLength1 = (someValue as string).length;
-console.log(strLength1); // Output: 11
-
-// reference type---> object
-type Person = {
-  readonly company?: "JS ENCODER"; //literal type  and optional type // readonly type when it not be changeable
-  name: string;
-  age?: number; //optional properties
-  isStudent: boolean;
-};
-
-let person: Person = {
-  company: "JS ENCODER",
-  name: "John Doe",
-  age: 25,
-  isStudent: true,
-};
-
-let user: Person = {
-  name: "nissan",
-  isStudent: false,
-};
-
-person.company = "JS ENCODER";
-
-const addBalance = (num1: number, num2: number) => num1 + num2;
-// same variable declare error though it on another page
-const poorUser = {
-  name: "Niloy",
-  balance: 1000,
-  addBalance(balance: number) {
-    return `my current balance is ${this.balance + balance}`;
-  },
-};
--- module1/src/spreadAndRestOparator.ts
+++ module1/src/spreadAndRestOparator.ts
@@ -1,9 +1,14 @@
 {
+  // how can i solve Cannot redeclare block-scoped variable 'poorUser'.ts(2451) this problem?
+
+  // i can add curly {} in th fle 1st line and last line and all content should be middle.
+
   const poorUser = {
     name: "nissan",
   };
+
+  //   learn spread operator in typescript
+  const friend: string[] = ["nissan", "rakib", "roki"];
+  const bestFriend:string[]=["azmin","Najim","Abir"]
+  friend.push(...bestFriend)
 }
-
-// how can i solve Cannot redeclare block-scoped variable 'poorUser'.ts(2451) this problem?
-
-// i can add curly {} in th fle 1st line and last line and all content should be middle.
